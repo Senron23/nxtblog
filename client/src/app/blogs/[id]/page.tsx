af0234c390e22a20ee2633a21cde2cf6d5df2610
Senron23 <ronsenbro1010@gmail.com>
2025-06-30 09:25:15 +0530

    update

--- client/src/app/blogs/[id]/page.tsx
+++ client/src/app/blogs/[id]/page.tsx
@@ -1,8 +1,5 @@
 import BlogDetailClient from "@/components/BlogdetailClient";
 
-interface PageParams {
-  id: string;
-}
 
 export default function Page({ params }: { params: { id: string} }) {
   return <BlogDetailClient id={params.id} />;
